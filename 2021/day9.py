a1095979c2000a82e557a60c071c4d626af6ac80
Elias Freider <elias.freider@kry.se>
2021-12-09 19:46:54 +0100

    Day 9

---
+++ 2021/day9.py
@@ -0,0 +1,66 @@
+import sys
+import re
+from collections import Counter, defaultdict, deque
+
+import numpy as np
+import networkx as nx
+from functools import reduce
+from operator import mul
+from lib.draw import draw, sparse_to_array
+import pyparsing as pp
+
+from lib.input import aoc_input, np_map, pb_input, tokens, chunks, lines, ints
+from lib.point import Point
+
+
+def part1():
+    src = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
+    src = aoc_input()
+    dirs = [np.array(a) for a in [(-1, 0), (1, 0), (0, 1), (0, -1)]]
+    m = np.array([[int(c) for c in w] for w in lines(src)])
+    risk = 0
+    for (y, x), n in np.ndenumerate(m):
+        ismin = True
+        for d in dirs:
+            p = d + (y, x)
+            if ((p >= 0) & (p < m.shape)).all():
+                if n >= m[p[0], p[1]]:
+                    ismin = False
+        if ismin:
+            risk += 1 + n
+
+    print(risk)
+
+
+def part2():
+    src = aoc_input()
+    dirs = [np.array(a) for a in [(-1, 0), (1, 0), (0, 1), (0, -1)]]
+    m = np.array([[int(c) for c in w] for w in lines(src)])
+
+    def rec(p, col):
+        m[p] = col
+        for d in dirs:
+            t = d + p
+            if ((t >= 0) & (t < m.shape)).all() and m[tuple(t)] < 9:
+                rec(tuple(t), col)
+
+    nextcol = 10
+    for pos, col in np.ndenumerate(m):
+        if col < 9:
+            rec(pos, nextcol)
+            nextcol += 1
+
+    counts = Counter(m[m != 9])
+    print(np.product(sorted(counts.values())[-3:]))
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
+
+
+
